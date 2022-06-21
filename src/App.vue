f1d3d73951e106af3daab93db446fd1314a3dbac
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2022-06-21 09:05:42 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,147 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="title">Advice #{{id}}</h1><!-- Advice ID goes here -->
+    <MessageVue :message="dados" />
+    <picture class="entalhe">
+      <source media="(max-width: 425px)" srcset="@/assets/pattern-divider-mobile.svg" />
+      <img src="@/assets/pattern-divider-desktop.svg" alt="" />
+    </picture>
+    <button class="btnGerar" @click="getRandomAdvice"><img src="@/assets/icon-dice.svg" alt=""></button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MessageVue from './components/Message.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MessageVue
+  },
+  data (){
+    return{
+      dados: null,
+      id: null,
+    }
+  },
+  methods:{
+    async getRandomAdvice(){
+      let spinnerElement = document.querySelector(".spinner").classList;
+      let messageElement = document.querySelector(".message").classList;
+
+      messageElement.add("d-none");
+      spinnerElement.remove("d-none");
+      
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      this.dados = data.slip.advice;
+      this.id = data.slip.id;
+
+      messageElement.remove("d-none");
+      spinnerElement.add("d-none");
+    }
+  },
+  mounted(){
+      this.getRandomAdvice()
   }
+  
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+
+:root{
+  --LightCyan: hsl(193, 38%, 86%);
+  --NeonGreen: hsl(150, 100%, 66%);
+  --GrayishBlue:  hsl(217, 19%, 38%);
+  --DarkGrayishBlue: hsl(217, 19%, 24%);
+  --DarkBlue: hsl(218, 23%, 16%);
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100vw;
+  min-height: 100vh;
+  font-family: 'Manrope', sans-serif;
+  font-weight: 800;
+  color:var(--LightCyan);
+  background-color: var(--DarkBlue);
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container{
+    display: flex;
+    flex-direction: column;
+    width: 30rem;
+    text-align: center;
+    border-radius: 15px;
+    background-color:var(--DarkGrayishBlue);
+    padding: 24px;
+    position: relative;
+    transition: height 0.4s;
+
+    @media (max-width: 450px){ margin: 1rem; }
+
+    .title{
+      text-transform: uppercase;
+      font-size: 12px;
+      color:var(--NeonGreen);
+      letter-spacing: 6px;
+      margin: 16px 0;
+
+      @media (max-width: 450px){ font-size: 10px; }
+
+    }
+
+    .entalhe{
+      width: 100%;
+      margin: 2rem 0;
+
+      @media (max-width: 450px){ margin: 1rem 0 2rem 0; }
+
+      img{
+          width: 100%;
+      }
+    }
+
+    .btnGerar{
+      width:4.2rem;
+      height: 4.2rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      position: absolute;
+      bottom: -2rem;
+      border-radius: 250px;
+      border: none;
+      background-color:var(--NeonGreen);
+      align-self: center;
+      cursor: pointer;
+      transition: 0.4s;
+
+      @media (max-width: 450px){ width: 3.8rem; height: 3.8rem;}
+
+      &:hover{
+        box-shadow: 0px 0px 20px 4px #52ffa8;
+        -webkit-box-shadow: 0px 0px 20px 4px #52FFA8; 
+      }
+
+    }
+  }
+
 }
+
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,53 @@
+<template>
+
+<div class="message-group">
+    <div class="spinner"></div>
+    <div class="message">"{{ message }}"</div>
+</div> 
+
+</template>
+
+<script>
+export default {
+    name: "Message",
+    props:{
+        message: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .message-group{
+        font-size: 28px;
+        font-weight: 800;
+        margin: 1.5rem 0;
+        transition: height 0.4s;
+
+        @media (max-width: 450px){ font-size: 22px; margin: 1rem 0;}
+
+        .spinner {
+            border: 8px solid rgba(0, 0, 0, 0.1);
+            border-left-color: var(--NeonGreen);
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+            animation: spin 1s linear infinite;
+            margin: auto;
+
+        }
+
+        .spinner,
+        .message{
+            &.d-none{
+                display: none;
+            }
+
+        }
+
+        @keyframes spin {
+            to {
+                transform: rotate(360deg);
+            }
+        }
+    }
+</style>
